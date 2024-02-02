f65c57fb97815e400956d993ebe9306c8aa83224
aruzaev <ruzaev.artem21@gmail.com>
2024-02-02 12:57:29 -0500

    Reverse string code

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let weirdString = string.split("").reverse();
+    weirdString = weirdString.join("");
+    return weirdString;
 };
 
 // Do not edit below this line
