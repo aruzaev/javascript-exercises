42249755a6c61a580b90729c2c8e3ba5d138f37f
aruzaev <ruzaev.artem21@gmail.com>
2024-02-02 12:44:33 -0500

    Add repeatString code

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let combinedString = "";
+    if (num < 0) return "ERROR";
+    for (let i = 0; i < num; ++i) {
+        combinedString += string
+    }
 
+    return combinedString;
 };
 
 // Do not edit below this line
