a35c3f50465dd809f0455ca50e3cd391b97c81ac
aruzaev <ruzaev.artem21@gmail.com>
2024-02-03 12:53:43 -0500

    Add sum all code

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  let max = Math.max(a, b);
+  let min = Math.min(a, b);
+  let sum = 0;
+  if (a < 0 || b < 0) return "ERROR";
 
+  //if (a < b) [b, a] = [a, b];
+  if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
